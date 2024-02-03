b645cb443bf98758404e1343e2b9aa27d8e1219b
Yashwanth635 <95156397+Yashwanth635@users.noreply.github.com>
2024-02-03 17:50:58 +0530

    Update NewsList.js

--- src/Components/NewsList.js
+++ src/Components/NewsList.js
@@ -1,8 +1,7 @@
-import { useState } from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
-
-import useNewsData from "../hooks/useNewsData";
-import CustomPagination from "./CustomPagination";
+import React, { useState } from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import useNewsData from '../hooks/useNewsData';
+import CustomPagination from './CustomPagination';
 
 const NewsList = (props) => {
   const { category, searchTerm } = props;
@@ -31,12 +30,14 @@ const NewsList = (props) => {
     <Container>
       <Row>
         {currentArticles?.map((article) => (
-          <Col xs={12} md={6} lg={4} key={article.url}>
-            <Card>
-              <Card.Img src={article.image} variant="top" />
-              <Card.Body>
-                <Card.Title>{article.title}</Card.Title>
-                <Card.Text>{article.description}</Card.Text>
+          <Col xs={12} md={6} lg={4} key={article.url} className="mb-4">
+            <Card style={{ height: '100%' }}>
+              <Card.Img src={article.image} variant="top" style={{ height: '200px', objectFit: 'cover' }} />
+              <Card.Body className="d-flex flex-column justify-content-between">
+                <div>
+                  <Card.Title>{article.title}</Card.Title>
+                  <Card.Text>{article.description}</Card.Text>
+                </div>
                 <Card.Link href={article.url}>Read More</Card.Link>
               </Card.Body>
             </Card>
@@ -54,3 +55,12 @@ const NewsList = (props) => {
 };
 
 export default NewsList;
+
+
+
+
+
+
+
+
+
